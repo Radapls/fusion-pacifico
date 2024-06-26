3b6c0d232ae72d42fdf796e6d552bb20110d2423
Juan Felipe Rada <radapls8@gmail.com>
2024-06-26 14:25:06 -0300

    first commit

---
+++ javascript/dictionary.js
@@ -0,0 +1,10 @@
+var dict = {      // 
+    "Menu": {
+      pt: "Menu",
+      en: "TEST"
+    },
+    "Download plugin": {
+      pt: "Descarregar plugin",
+      en: "Download plugin"
+    }
+  };
